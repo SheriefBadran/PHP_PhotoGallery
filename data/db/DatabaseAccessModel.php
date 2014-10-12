3dbc30283b4ee09afae9537558ea91521678b354
SheriefBadran <sheriefbadran@Sheriefs-MacBook-Air.local>
2014-10-12 20:54:47 +0200

    DatabaseAccessModel refactored. Generalized insert to database broken into small testable and reusable pieces.

--- data/db/DatabaseAccessModel.php
+++ data/db/DatabaseAccessModel.php
@@ -4,62 +4,87 @@ require_once(HelperPath.DS.'db/DB_Factory.php');
 
 	class DatabaseAccessModel {
 
+		private static $dbUsername = "root";
+		private static $dbPassword = "root";
+		private static $connectionString = 'mysql:host=localhost;dbname=PhotoGallery';
 		protected $dbFactory;
+		private static $fields = "fields";
+		private static $paramPlaceHolder = "paramPlaceHolder";
 
 		public function __construct () {
 
 			$dbAbstactFactory = new DB_Factory();
-			$this->dbFactory = $dbAbstactFactory::getFactory('root', 'root', 'mysql:host=localhost;dbname=PhotoGallery');
+			$this->dbFactory = $dbAbstactFactory::getFactory(self::$dbUsername, self::$dbPassword, self::$connectionString);
 		}
 
-		public function insert ($object) {
+		// Helper methods.
+		protected function retrieveObjectGetters (Array $objectGetters, Array $objectMethods) {
 
-			$getters = array();
-			$objectMethods = get_class_methods($object);
-
-			$properties = array_keys(static::$tblFieldGetters);
-			$objectGetters = array_values(static::$tblFieldGetters);
-
+			$getterMethods = array();
 			foreach ($objectGetters as $objectGetter) {
 
 				foreach ($objectMethods as $objectMethod) {
 					
 					if ($objectMethod === $objectGetter) {
 						
-						$getters[] = $objectMethod;
+						$getterMethods[] = $objectMethod;
 					}
 				}
 			}
 
+			return $getterMethods;
+		}
+
+		protected function getObjectAttributeValues (Array $getterMethods, $object) {
+
 			$params = array();
+			foreach ($getterMethods as $getter) {
+				
+				$params[] = call_user_func(array($object, $getter));
+			}
+
+			return $params;
+		}
+
+		protected function composePDOqueryComponents (Array $objectProperties) {
+
 			$tblFields = " (";
 			$paramString = "(";
 
 			$methods = get_class_methods(static::$repositoryType);
 
-			for ($i = 0; $i < count($properties); $i++) {
+			for ($i = 0; $i < count($objectProperties); $i++) {
 				
-				$tblFields .= ($i === count($properties) - 1) ? $properties[$i] 
-																	 : $properties[$i] . ", ";
+				$tblFields .= ($i === count($objectProperties) - 1) ? $objectProperties[$i] 
+																	 : $objectProperties[$i] . ", ";
 
-				$paramString .= ($i === count($properties) - 1) ? "?" : "?,";
+				$paramString .= ($i === count($objectProperties) - 1) ? "?" : "?,";
 			}
 
 			$tblFields .= ") ";
 			$paramString .= ")";
 
-			foreach ($getters as $getter) {
-				
-				$params[] = call_user_func(array($object, $getter));
-			}
+			return array(
+				self::$fields 			=> $tblFields,
+				self::$paramPlaceHolder => $paramString 
+			);
+		}
 
-			// echo "<pre>";
-			// print_r($params);
-			// echo "</pre>";
-			// die();
+		public function insert ($object) {
+
+			$objectMethods = get_class_methods($object);
+			$objectGetters = array_values(static::$tblFieldGetters);
+
+			$properties = array_keys(static::$tblFieldGetters);
+			
+
+			$getters = $this->retrieveObjectGetters($objectGetters, $objectMethods);
+			$queryComponents = $this->composePDOqueryComponents($properties);
+			$params = $this->getObjectAttributeValues($getters, $object);
+
 			$db = $this->dbFactory->createInstance();
 			$sql = "INSERT INTO " . static::$tblName;
-			$sql .= $tblFields . "VALUES " . $paramString;
+			$sql .= $queryComponents[self::$fields] . "VALUES " . $queryComponents[self::$paramPlaceHolder];
 			$query = $db->prepare($sql);
 			$query -> execute($params);
 		}
